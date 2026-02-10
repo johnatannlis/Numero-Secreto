dbc4e4d20dfb39c179acf529289b8ca550cb9c9c
Johnatan Lis <johnatannlis@gmail.com>
2026-02-09 22:28:22 -0300

    Alterando limite para 100 e ajustando mensagem com numeroLimite no exibirTextoNaTela

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um numero de 1 a 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1
 
@@ -16,7 +16,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
+exibirTextoNaTela('p', `Escolha um número de 1 a ${numeroLimite}`);
 }
 
 exibirMensagemInicial();
